8f2c05bf483a8119facf01cfb75a1db51aeed03f
matsumotoNaoma <imajin8284@gmail.com>
2024-06-07 23:22:34 +0900

    add sass refact

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Hello from './Hello';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+// import About from './About';
+// import Contact from './Contact';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Hello name="World" />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.scss';  
+
+const Home: React.FC = () => {
+  return <h2>Home Page</h2>;
+}
+
+export default Home;  
